c89e46bc7810db2b8aa24cdde936bda3f62d1ee2
Elliot Maisl <elliot.maisl@gmail.com>
2020-08-13 22:28:57 +0200

    🎨 Add a Logger

---
+++ src/Logger.js
@@ -0,0 +1,37 @@
+import chalk from 'chalk';
+
+class Logger {
+  /**
+   * Logs a message with a prefix and the date
+   * @param {string} msg - The message to log
+   */
+  log(msg) {
+    console.log(msg);
+  }
+
+  /**
+   * Shows an error with a prefix
+   * @param {string} msg - The message to log
+   */
+  error(msg) {
+    console.log(`${chalk.bgRed(' ✗ ')} ${msg}`);
+  }
+
+  /**
+   * Shows a success message with a prefix
+   * @param {string} msg - The message to log
+   */
+  success(msg) {
+    console.log(`${chalk.bgGreen(' ✔ ')} ${msg}`);
+  }
+
+  /**
+   * Shows an information message with a prefix
+   * @param {string} msg - The message to log
+   */
+  info(msg) {
+    console.log(`${chalk.black.bgCyan(' ℹ ')} ${msg}`);
+  }
+}
+
+export default Logger;
--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import inquirer from 'inquirer';
 import minimist from 'minimist';
 import ora from 'ora';
 
+import Logger from './Logger.js';
 import db from './database.js';
 import handleError from './handleError.js';
 import {
@@ -27,10 +28,10 @@ import {
 } from './steps.js';
 
 const argv = minimist(process.argv.slice(2));
+const logger = new Logger();
 const install = false;
 
 async function generateProject(answers, usedConfig = false) {
-  console.log('DEBUG: generateProject -> projectName', answers.projectName, typeof answers.projectName);
   const sameConfig = await searchForSameConfig(answers);
 
   const spinner = ora('Creating directory').start();
@@ -88,75 +89,75 @@ async function generateProject(answers, usedConfig = false) {
 
   spinner.succeed('Finished successfully!');
 
-  console.log('\n\n');
+  logger.log('\n\n');
   if (sameConfig && !usedConfig) {
-    console.log();
-    console.log(`${chalk.black.bgCyan(' ℹ ')} The exact same config is already saved under the name ${chalk.yellow(sameConfig[0].name)}.`);
-    console.log(`    Use ${chalk.grey(`nipinit -c ${sameConfig[0].name}`)} to use it directly, next time!`);
-    console.log();
+    logger.log('');
+    logger.log(`${chalk.black.bgCyan(' ℹ ')} The exact same config is already saved under the name ${chalk.yellow(sameConfig[0].name)}.`);
+    logger.log(`    Use ${chalk.grey(`nipinit -c ${sameConfig[0].name}`)} to use it directly, next time!`);
+    logger.log('');
   }
-  console.log(`${chalk.green.bold('➜')} ${chalk.underline.bold('Your project has been created successully!')}`);
-  console.log(`You can find it at ${chalk.cyan(`./${answers.projectName}/`)}!`);
-  console.log();
-  console.log(chalk.bold('There is a few more things you may want to do to be ready...'));
-  console.log(`    ${chalk.grey('-')} Set the contact method in CONTRIBUTING.md (search for "[INSERT CONTACT METHOD]")`);
-  console.log(`    ${chalk.grey('-')} Update the scripts in package.json to your linking or to use module you want`);
-  console.log(`    ${chalk.grey('-')} Add/fill/update some package.json entries, such as the repo, the description, keywords...`);
-  console.log(`    ${chalk.grey('-')} Update the TO-DO in README.md`);
-  console.log();
-  console.log(chalk.green('Have fun!'));
+  logger.log(`${chalk.green.bold('➜')} ${chalk.underline.bold('Your project has been created successully!')}`);
+  logger.log(`You can find it at ${chalk.cyan(`./${answers.projectName}/`)}!`);
+  logger.log('');
+  logger.log(chalk.bold('There is a few more things you may want to do to be ready...'));
+  logger.log(`    ${chalk.grey('-')} Set the contact method in CONTRIBUTING.md (search for "[INSERT CONTACT METHOD]")`);
+  logger.log(`    ${chalk.grey('-')} Update the scripts in package.json to your linking or to use module you want`);
+  logger.log(`    ${chalk.grey('-')} Add/fill/update some package.json entries, such as the repo, the description, keywords...`);
+  logger.log(`    ${chalk.grey('-')} Update the TO-DO in README.md`);
+  logger.log('');
+  logger.log(chalk.green('Have fun!'));
 }
 
 if (argv._[0] === 'config') {
   if (argv._[1] === 'ls') {
     const configurations = await db.get('configurations').value();
     if (configurations.length === 0) {
-      console.log(`${chalk.bgRed(' ✗ ')} No configuration found for nipinit.`);
+      logger.log(`${chalk.bgRed(' ✗ ')} No configuration found for nipinit.`);
     } else {
-      console.log(chalk.bold.underline(`Found ${configurations.length} configurations for nipinit:`));
+      logger.log(chalk.bold.underline(`Found ${configurations.length} configurations for nipinit:`));
       for (const config of configurations) {
-        console.log(`  ${chalk.grey('-')} ${config.name}`);
+        logger.log(`  ${chalk.grey('-')} ${config.name}`);
       }
-      console.log(chalk.italic(`You can have more informations about a config with ${chalk.grey('nipinit config info <config>')}`));
+      logger.log(chalk.italic(`You can have more informations about a config with ${chalk.grey('nipinit config info <config>')}`));
     }
   } else if (argv._[1] === 'remove') {
     const config = await db.get('configurations')
       .find({ name: argv._[2] })
       .value();
     if (!config) {
-      console.log(`${chalk.bgRed(' ✗ ')} The configuration ${argv._[2]} does not exist.`);
+      logger.log(`${chalk.bgRed(' ✗ ')} The configuration ${argv._[2]} does not exist.`);
     } else {
       await db.get('configurations')
         .remove({ name: config.name })
         .write();
-      console.log(`${chalk.bgGreen(' ✔ ')} The configuration ${config.name} was deleted successfully!`);
+      logger.log(`${chalk.bgGreen(' ✔ ')} The configuration ${config.name} was deleted successfully!`);
     }
   } else if (argv._[1] === 'info') {
     const config = await db.get('configurations')
       .find({ name: argv._[2] })
       .value();
     if (!config) {
-      console.log(`${chalk.bgRed(' ✗ ')} The configuration ${argv._[2]} does not exist.`);
+      logger.log(`${chalk.bgRed(' ✗ ')} The configuration ${argv._[2]} does not exist.`);
     } else {
-      console.log(chalk.bold.underline(`Informations about the configuration ${chalk.cyan(config.name)}:`));
-      console.log(`    ${chalk.grey('-')} User Name: ${chalk.cyan(config.userName)}`);
-      console.log(`    ${chalk.grey('-')} Init Git: ${config.git ? chalk.green('Yes') : chalk.red('No')}`);
-      if (config.git) console.log(`      ${chalk.grey('-')} Init Github files: ${config.github ? chalk.green('Yes') : chalk.red('No')}`);
-      console.log(`    ${chalk.grey('-')} License: ${chalk.cyan(config.license)}`);
-      console.log(`    ${chalk.grey('-')} Use ES Modules: ${config.module ? chalk.green('Yes') : chalk.red('No')}`);
-      console.log(`    ${chalk.grey('-')} Use babel: ${config.babel ? chalk.green('Yes') : chalk.red('No')}`);
-      console.log(`    ${chalk.grey('-')} Use ESLint: ${config.eslint !== "I don't want to use ESLint" ? chalk.green('Yes') : chalk.red('No')}`);
-      if (config.eslint !== "I don't want to use ESLint") console.log(`      ${chalk.grey('-')} ESLint configuration: ${chalk.cyan(config.eslint)}`);
+      logger.log(chalk.bold.underline(`Informations about the configuration ${chalk.cyan(config.name)}:`));
+      logger.log(`    ${chalk.grey('-')} User Name: ${chalk.cyan(config.userName)}`);
+      logger.log(`    ${chalk.grey('-')} Init Git: ${config.git ? chalk.green('Yes') : chalk.red('No')}`);
+      if (config.git) logger.log(`      ${chalk.grey('-')} Init Github files: ${config.github ? chalk.green('Yes') : chalk.red('No')}`);
+      logger.log(`    ${chalk.grey('-')} License: ${chalk.cyan(config.license)}`);
+      logger.log(`    ${chalk.grey('-')} Use ES Modules: ${config.module ? chalk.green('Yes') : chalk.red('No')}`);
+      logger.log(`    ${chalk.grey('-')} Use babel: ${config.babel ? chalk.green('Yes') : chalk.red('No')}`);
+      logger.log(`    ${chalk.grey('-')} Use ESLint: ${config.eslint !== "I don't want to use ESLint" ? chalk.green('Yes') : chalk.red('No')}`);
+      if (config.eslint !== "I don't want to use ESLint") logger.log(`      ${chalk.grey('-')} ESLint configuration: ${chalk.cyan(config.eslint)}`);
     }
   } else if (argv.h || argv.help) {
-    console.log(`${chalk.bold('nipinit config')} allows you to manage the configurations.`);
-    console.log();
-    console.log('Available commands:');
-    console.log(`  ${chalk.grey('-')} ${chalk.bold('nipinit config ls')} ${chalk.italic.grey('List all existing configurations')}`);
-    console.log(`  ${chalk.grey('-')} ${chalk.bold('nipinit config info <config>')} ${chalk.italic.grey('Get informations about a configuration')}`);
-    console.log(`  ${chalk.grey('-')} ${chalk.bold('nipinit config remove <config>')} ${chalk.italic.grey('Remove a configuration')}`);
+    logger.log(`${chalk.bold('nipinit config')} allows you to manage the configurations.`);
+    logger.log('');
+    logger.log('Available commands:');
+    logger.log(`  ${chalk.grey('-')} ${chalk.bold('nipinit config ls')} ${chalk.italic.grey('List all existing configurations')}`);
+    logger.log(`  ${chalk.grey('-')} ${chalk.bold('nipinit config info <config>')} ${chalk.italic.grey('Get informations about a configuration')}`);
+    logger.log(`  ${chalk.grey('-')} ${chalk.bold('nipinit config remove <config>')} ${chalk.italic.grey('Remove a configuration')}`);
   } else {
-    console.log(`${chalk.bgRed(' ✗ ')} Unkown argument: "${argv._[1]}". Use ${chalk.grey('nipinit config --help')} for help`);
+    logger.log(`${chalk.bgRed(' ✗ ')} Unkown argument: "${argv._[1]}". Use ${chalk.grey('nipinit config --help')} for help`);
   }
 } else {
   const configArgument = argv.c ?? argv.config;
@@ -218,18 +219,15 @@ if (argv._[0] === 'config') {
     config.projectName = projectName;
 
     if (!config) {
-      console.log(`${chalk.bgRed(' ✗ ')} This configuration does not exist!`);
-      console.log(`    Run ${chalk.grey('nipinit config ls')} to see all available configurations.`);
+      logger.log(`${chalk.bgRed(' ✗ ')} This configuration does not exist!`);
+      logger.log(`    Run ${chalk.grey('nipinit config ls')} to see all available configurations.`);
     } else {
       generateProject(config, true);
     }
   } else {
     inquirer
       .prompt(questions)
-      .then((a) => {
-        console.log('DEBUG: prompt -> projectName', a.projectName, typeof a.projectName);
-        generateProject(a);
-      })
+      .then(generateProject)
       .catch(handleError);
   }
 }
