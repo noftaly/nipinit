4ebe5b8097af0e73d3408a2ff6280f49f40d79ee
Elliot Maisl <elliot.maisl@gmail.com>
2020-10-21 11:53:56 +0200

    🎨  Improve function typing

--- src/utils/handleError.ts
+++ src/utils/handleError.ts
@@ -1,6 +1,4 @@
-// It does not return anything, it throws an error (see #12129: https://github.com/microsoft/TypeScript/issues/13219)
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default function handleError(error: Error) {
+export default function handleError(error: Error): void {
   console.error('An error occured while prompting questions');
   const err = new Error(error.message);
   err.stack = error.stack;
