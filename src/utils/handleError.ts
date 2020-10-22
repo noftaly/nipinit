5fde8d4aff91a9c9f960e81cacafdd6384388114
Elliot Maisl <elliot.maisl@gmail.com>
2020-10-22 22:44:45 +0200

    🔥 Remove unused file

--- src/utils/handleError.ts
+++
@@ -1,6 +0,0 @@
-export default function handleError(error: Error): void {
-  console.error('An error occured while prompting questions');
-  const err = new Error(error.message);
-  err.stack = error.stack;
-  throw err;
-}
