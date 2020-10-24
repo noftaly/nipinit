c671adcc05a1736e6a59d2088f27cf03105e7ab6
Elliot Maisl <elliot.maisl@gmail.com>
2020-10-24 20:18:17 +0200

    🚸  Improve error infos + show message on invalid argument

--- src/main.ts
+++ src/main.ts
@@ -18,32 +18,39 @@ const cli = new CLI();
 program
   .version(pkg.version, '-v, --version', 'Output the nipinit version')
   .option('-p, --preset <string>', 'Create a new project with a preset')
+  .option('-d, --dump-error', 'Show the full error when one occurs')
   .option('--no-modules', 'Create a new project without installing node modules')
-  .option('--dump-error, -d', 'Show the full error when one occurs')
   .option('--no-color', 'Create a new project without showing colors in the CLI')
   .action(async (options: CLIOptions, command?: string) => {
-    // If it is a subcommand, then stop here and let the other actions take care of it
-    if (command) return;
-
-    try {
-      await cli.startPrompting(options.preset, options.modules);
-    } catch (err) {
-      logger.empty(2);
-      logger.error('An error occured while using nipinit... More information are available below this message.');
-      if (options.dumpError) {
-        console.log('Used preset:', options.preset || 'None');
-        console.log('Install modules:', options.modules || true);
-        console.log('Node version:', process.version);
-        console.log('Nipinit version:', pkg.version);
-        console.log('Error: --------------');
-        console.error(err);
-      } else {
-        logger.error(err.message);
-        const stacks = err.stack.split('\n').slice(1, 3);
-        for (const stack of stacks)
-          logger.error(stack);
+    if (command && command !== 'presets') {
+      logger.error('Unkown argument. Run nipinit -h for help');
+    } else if (!command) {
+      try {
+        await cli.startPrompting(options.preset, options.modules);
+      } catch (err) {
+        logger.empty(2);
+        logger.error('An error occured while using nipinit... More information are available below this message.');
+        logger.error('If you think this is a bug, please report it here: https://github.com/noftaly/nipinit, with the "bug" issue template filled correctly.');
+        if (options.dumpError) {
+          if (options.preset) {
+            const preset = cli.presetManager.findByName(options.preset);
+            const values = Object.entries(preset).map(([name, value]) => `${name}: ${value}`);
+            logger.info(`Used preset: ${values}`);
+          } else {
+            logger.info('Used preset: None');
+          }
+          logger.info(`Install modules: ${options.modules || true}`);
+          logger.info(`Node version: ${process.version}`);
+          logger.info(`Nipinit version: ${pkg.version}`);
+          console.error(err);
+        } else {
+          logger.error(err.message);
+          const stacks = err.stack.split('\n').slice(1, 3);
+          for (const stack of stacks)
+            logger.error(stack);
+        }
+        process.exit(1);
       }
-      process.exit(1);
     }
   });
 
