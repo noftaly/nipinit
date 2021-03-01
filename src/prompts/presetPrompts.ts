04cf97fbf93607f55235b4b94547abbcc7974c57
Elliot Maisl <elliot.maisl@gmail.com>
2021-03-01 19:29:30 +0100

    💥  Forbid spaces in preset name

--- src/prompts/presetPrompts.ts
+++ src/prompts/presetPrompts.ts
@@ -16,7 +16,9 @@ export const presetName = (presetManager: PresetManager, answers: GeneralAnswers
   message: 'What name do you want to give to this preset?',
   default: (): string => presetManager.createName(answers.userName),
   when: (prefs: PresetCreationAnswers): boolean => prefs.save,
-  validate: (input: string): boolean | string => {
+  validate: (input: string): string | true => {
+    if (input.includes(' '))
+      return 'The preset name cannot contain spaces.';
     if (input.length === 0)
       return 'The preset name has to contain at least 1 character.';
     if (presetManager.findByName(input))
